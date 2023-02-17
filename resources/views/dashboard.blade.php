bffbccf8dae1f137fa4cfba53fc56f76c5fb8aec
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-02-17 17:26:08 +0100

    Create welcome text with user name

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,7 +14,7 @@
                     </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __("Welcome $userName->name") }}
                 </div>
             </div>
         </div>
